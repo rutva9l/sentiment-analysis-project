58d4bb082f5d4baa8cc742a62d06fd89e6e4bafa
rutva9l <rutva943@gmail.com>
2024-05-26 21:15:37 +0530

    front

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+// import Papa from "papaparse"
+import { useState } from "react"
+
+const allowedExtensions = ["csv"]
 
 const pageStyles = {
   color: "#232129",
@@ -90,63 +94,132 @@ const badgeStyle = {
   lineHeight: 1,
 }
 
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
 const IndexPage: React.FC<PageProps> = () => {
+  const [text, setText] = useState("")
+
+  // It state will contain the error when
+  // correct file extension is not used
+  const [error, setError] = useState("");
+
+  // It will store the file uploaded by the user
+  const [file, setFile] = useState<File>();
+
+  // This function will be called when
+  // the file input changes
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+    setText(e.target.value)
+  }
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setError("");
+
+    // Check if user has entered the file
+    if (e.target.files.length) {
+      const inputFile = e.target.files[0];
+      console.log(e.target.files)
+
+      // Check the file extensions, if it not
+      // included in the allowed extensions
+      // we show the error
+      const fileExtension =
+        inputFile?.type.split("/")[1];
+      if (
+        !allowedExtensions.includes(fileExtension)
+      ) {
+        setError("Please input a csv file");
+        return;
+      }
+
+      // If input type is correct set the state
+      setFile(inputFile);
+    }
+  };
+  const handleParse = () => {
+
+    // If user clicks the parse button without
+    // a file we show a error
+    if (!file && !text) return alert("Enter a valid file or text");
+
+    var csvFileInput = document.getElementById("csvFileInput");
+    var textInput = document.getElementById("textInput");
+    var predictionResult = document.getElementById("predictionResult");
+    var graphContainer = document.getElementById("graphContainer");
+
+    // Initialize a reader which allows user
+    // to read any file or blob.
+    if (file) {
+
+      var formData = new FormData();
+      formData.append("file", file);
+
+      fetch("http://127.0.0.1:5000/predict", {
+        mode: "no-cors",
+        method: "POST",
+        body: formData
+      })
+        .then(response => {
+          if (response.headers.get('X-Graph-Exists') === 'true') {
+            console.log("Graph")
+            // var graphData = response.headers.get('X-Graph-Data');
+            // displayGraph(graphData);
+          }
+
+          return response.blob();
+        })
+        .then(blob => {
+          console.log("Blob:", blob);
+
+          console.log(download)
+
+          // document.getElementById("downloadBtn").style.display = "block";
+          // document.getElementById("downloadBtn").onclick = function () {
+            console.log("Downloading...");
+            // var url = URL.createObjectURL(blob);
+            console.log("URL:", url);
+
+            // var a = document.createElement("a");
+            // a.href = url;
+            // a.download = "Predictions.csv";
+            // document.body.appendChild(a);
+            // a.click();
+            // document.body.removeChild(a);
+          // };
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });
+    }
+    else if (text) {
+      // Predict on single sentence
+      console.log("send rext")
+      fetch("http://127.0.0.1:5000/predict", {
+        mode: "no-cors",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ "text": text.trim() })
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          // predictionResult.innerHTML = "Predicted sentiment: " + data.prediction;
+        });
+    }
+  };
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
+        Sentiment
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
+        <span style={headingAccentStyles}> Analysis</span>
       </h1>
       <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
+        A site to predict the sentiment of any statement and provide graphical sentiment analysis for csv files.
+        {/* Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
+        update in real-time. 😎 */}
       </p>
       <ul style={doclistStyles}>
         {docLinks.map(doc => (
@@ -160,26 +233,34 @@ const IndexPage: React.FC<PageProps> = () => {
           </li>
         ))}
       </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
+      <div className="container">
+        {/* <div>
+          <label
+            htmlFor="csvInput"
+            style={{ display: "block" }}
+          >
+            Enter CSV File
+          </label>
+          <input
+            onChange={handleFileChange}
+            id="csvInput"
+            name="file"
+            type="File"
+          />
+        </div> */}
+        <div>
+          <label htmlFor="textInput">
+            Enter your text
+          </label>
+          <input type="text" id="textInput" name="text" onChange={handleTextChange} />
+        </div>
+        <div>
+          <button onClick={handleParse}>
+            Submit
+          </button>
+        </div>
+      </div>
+
       <img
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
